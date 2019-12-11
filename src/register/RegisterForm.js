33be216b4fd85e3b9543d0d92de6c9f6fc1d1b27
Dang Van Duc <dangvanduc0@gmail.com>
2019-12-11 10:03:22 +0700

    onBlur server side validator

--- src/register/RegisterForm.js
+++ src/register/RegisterForm.js
@@ -1,39 +1,32 @@
-import React from 'react';
-import { Field, reduxForm } from 'redux-form';
-import validateInput from './validate';
-import { Button } from 'react-bootstrap';
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+import validate from './validate'
+import asyncValidate from './asyncValidate'
 
-const renderField = ({ input, label, type, meta: { touched, error } }) => (
-  <div className="form-group">
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { asyncValidating, touched, error }
+}) => (
+  <div>
     <label>{label}</label>
-    <div>
-      <input {...input} placeholder={label} type={type} />
+    <div className={asyncValidating ? 'async-validating' : ''}>
+      <input {...input} type={type} placeholder={label} />
+      {touched && error && <span>{error}</span>}
     </div>
-    {touched && error && <span className="text-danger">{error}</span>}
   </div>
-);
+)
 
-const SubmitValidationForm = props => {
-  const { error, handleSubmit, pristine, reset, submitting } = props;
+const AsyncValidationForm = props => {
+  const { handleSubmit, pristine, reset, submitting } = props
   return (
-    <form className="form-horizontal" onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <Field
-        name="firstName"
+        name="username"
         type="text"
         component={renderField}
-        label="first name"
-      />
-      <Field
-        name="lastName"
-        type="text"
-        component={renderField}
-        label="last name"
-      />
-       <Field
-        name="email"
-        type="text"
-        component={renderField}
-        label="email"
+        label="Username"
       />
       <Field
         name="password"
@@ -41,18 +34,21 @@ const SubmitValidationForm = props => {
         component={renderField}
         label="Password"
       />
-      {error && <strong>{error}</strong>}
       <div>
-        <Button type="submit" disabled={submitting}>register</Button>
-        <Button type="button" disabled={pristine || submitting} onClick={reset}>
+        <button type="submit" disabled={submitting}>
+          Sign Up
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
           Clear Values
-        </Button>
+        </button>
       </div>
     </form>
-  );
-};
+  )
+}
 
 export default reduxForm({
-  form: 'submitValidation', // a unique identifier for this form
-  validate: validateInput,
-})(SubmitValidationForm);
+  form: 'asyncValidation', // a unique identifier for this form
+  validate,
+  asyncValidate,
+  asyncBlurFields: ['username']
+})(AsyncValidationForm)
--- src/register/validate.js
+++ src/register/validate.js
@@ -1,22 +1,10 @@
-import isEmpty from 'lodash/isEmpty';
-
- const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
 const validateInput = (values) => {
   const errors = {};
-  if(isEmpty(values.firstName)) {
-    errors.firstName = { _error: 'Required' };
+  if (!values.username) {
+    errors.username = 'Required'
   }
-  if(isEmpty(values.lastName)) {
-    errors.lastName = { _error: 'Required' };
-  }
-  if(isEmpty(values.email)) {
-    errors.email = { _error: 'Required' };
-  } else if(!regexEmail.test(values.email)){
-    errors.email = { _error: 'Email invalid' };
-  }
-  if(isEmpty(values.password)) {
-      errors.password = { _error: 'Required' };
+  if (!values.password) {
+    errors.password = 'Required'
   }
   return errors;
 };
