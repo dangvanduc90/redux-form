4c2032a7aedb781081adf0d645b1df42260cb53f
Dang Van Duc <dangvanduc0@gmail.com>
2019-12-25 14:31:07 +0700

    Revert "config eslint"
    This reverts commit 55632f0aba64e4c7c81558b9e6b0815c0819540a.

--- src/register/asyncValidate.js
+++ src/register/asyncValidate.js
@@ -1,11 +1,9 @@
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-
-const asyncValidate = (values /* , dispatch */) => {
+const asyncValidate = (values /*, dispatch */) => {
   return sleep(1000).then(() => {
     // simulate server latency
     if (['john', 'paul', 'george', 'ringo'].includes(values.username)) {
-      // eslint-disable-next-line no-throw-literal
       throw { username: 'That username is taken' }
     }
   })
