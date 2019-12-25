a64c730aa273509018a1a80a6598093250aa7817
Dang Van Duc <dangvanduc0@gmail.com>
2019-12-25 16:31:24 +0700

    set env

--- src/common/Button.js
+++ src/common/Button.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 class Button extends Component {
   render() {
-    return <button>Button common</button>
+    return <button>Button {process.env.REACT_APP_NOT_SECRET_CODE + ' ' + process.env.DB_HOST}</button>
   }
 }
 export default Button; // Don’t forget to use export default!
